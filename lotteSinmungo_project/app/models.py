14ba47f814e37359b6817518a265365ea2aebda4
Gobans <audghks4452@gmail.com>
2020-10-11 15:33:27 +0900

    resolving #6

--- lotteSinmungo_project/app/models.py
+++ lotteSinmungo_project/app/models.py
@@ -4,3 +4,15 @@ class Problem (models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     updated_at = models.DateTimeField(auto_now=True) 
+
+class myUser(models.Model): #장고에서 제공하는 models.Model를 상속받아야한다.
+    objects = models.Manager()
+    username = models.CharField(max_length=64,verbose_name = '사용자명')
+    password = models.CharField(max_length=64,verbose_name = '비밀번호')
+    registered_dttm = models.DateTimeField(auto_now_add=True,verbose_name='등록시간')
+    
+    def __str__(self): # 이 함수 추가
+        return self.username  # User object 대신 나타낼 문자 
+
+    class Meta:
+        db_table = 'test_user'
--- lotteSinmungo_project/lotteSinmungo/urls.py
+++ lotteSinmungo_project/lotteSinmungo/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('problemList/',views.problemList, name = 'problemList'),
     path('writing/',views.writing, name = 'writing'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
 ]
