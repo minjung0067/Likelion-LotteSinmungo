213039f7a70ae9aa643e9e73fb591be7814589e8
jaehoonkimm <jaehoonkim.dev@gmail.com>
2020-10-11 15:56:13 +0900

    add: Add Problem_solutions model class in models.py

--- lotteSinmungo_project/app/models.py
+++ lotteSinmungo_project/app/models.py
@@ -4,3 +4,9 @@ class Problem (models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     updated_at = models.DateTimeField(auto_now=True) 
+
+class Problem_solutions(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    original_date = models.DateTimeField(auto_now=False)
+    upload_date = models.DateTimeField(auto_now=True)
