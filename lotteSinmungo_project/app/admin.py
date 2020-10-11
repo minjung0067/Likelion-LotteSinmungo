df1ba738f39f52cdb8551201d74408b0865dab5b
jaehoonkimm <jaehoonkim.dev@gmail.com>
2020-10-11 16:49:49 +0900

    add: Add admin site register code in admin.py (Problem_solution model)

--- lotteSinmungo_project/app/admin.py
+++ lotteSinmungo_project/app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Problem, myUser
+from .models import Problem, myUser, Problem_solutions
 
 class UserAdmin(admin.ModelAdmin) :
     list_display = ('username', 'password')
 
-
+admin.site.register(Problem_solutions)
 admin.site.register(Problem)
 admin.site.register(myUser, UserAdmin) 
