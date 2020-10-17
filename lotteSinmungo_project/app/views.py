cf5b9220c5e5391e004dee6c992cce2df3147c12
minjung <minjung0067@naver.com>
2020-10-17 12:18:07 +0900

    fix notification and solution page

--- lotteSinmungo_project/app/views.py
+++ lotteSinmungo_project/app/views.py
@@ -31,9 +31,6 @@ def problemDetail(request, problem_detail_id):
 
 def problemList(request):
     problem_list_item = Problem.objects.all()
-    if request.user.is_authenticated:
-        user = request.user
-        user.notifications.mark_all_as_read()
     """--- 랭킹 ---"""
     problem_trending = Problem.objects.order_by('-like_count', '-updated_at')
     problem_trending = problem_trending[:10]
@@ -61,22 +58,22 @@ def problemList(request):
 
 def solution(request):
     solution_item = Solution.objects.all()
-    if request.user.is_superuser:
-        user_id = request.user.id
-        user = request.user #알림 보낼 관리자
-        recipients = myUser.objects.all()  #알림 받을 사람들
-        if request.method == "POST":
-            filled_form = SolutionForm(request.POST)
-            if filled_form.is_valid():
-                post = filled_form.save(commit=False)
-                post.userid = user_id
-                post.save()
-                if user in recipients:
-                    unread_messages = user.notifications.unread()
-                    notify.send (user, recipient = recipients, verb ='님께서 새로운 숙제를 작성하셨습니다 (●''●)')
-            
-        sol_form = SolutionForm()
-        return render(request, 'solution.html', {'sol_form':sol_form})
+    if request.user.is_authenticated:
+        user = request.user
+        user.notifications.mark_all_as_read()
+        if request.user.is_superuser:
+            user_id = request.user.id
+            recipients = myUser.objects.all()  #알림 받을 사람들
+            if request.method == "POST":
+                filled_form = SolutionForm(request.POST)
+                if filled_form.is_valid():
+                    post = filled_form.save(commit=False)
+                    post.userid = user_id
+                    post.save()
+                    notify.send (user, recipient = recipients, verb ='님이 새로운 문제를 해결했어요')
+            sol_form = SolutionForm()
+            return render(request, 'solution.html', {'sol_form':sol_form,'solution_item':solution_item})
+        return render(request, 'solution.html', {'solution_item':solution_item})
     else:
         return render(request, 'solution.html', {'solution_item':solution_item})
 
@@ -94,9 +91,6 @@ def writing(request):
             post = filled_form.save(commit=False)
             post.userid = user_id
             post.save()
-            if user in recipients:
-                unread_messages = user.notifications.unread()
-                notify.send (user, recipient = recipients, verb ='님께서 새로운 숙제를 작성하셨습니다 (●''●)')
             return redirect('problemList') #problemList 중에서도 최신 순으로 나열되어 있는 페이지를 보여주는 게 좋을듯 (나중에 추가하자)
     prb_form = ProblemForm()
     return render(request, 'writing.html', {'prb_form':prb_form})
@@ -177,13 +171,13 @@ def signout(request): #로그아웃 기능
 @login_required
 def mypage(request): #마이페이지 #주희가 수정 중!
     user = request.user
-    # unread_messages = user.notifications.unread()
+    unread_messages = user.notifications.unread()
     profile = myUser.objects.get(id=user.id)
     #내가 쓴 글
     my_problem_item = Problem.objects.filter(userid = user.id)
     #내가 좋아하는 게시물
     # check_like_post = profile.like_problems.objects.all()
-    return render(request, 'mypage.html', {'my_problem_item': my_problem_item})
+    return render(request, 'mypage.html', {'my_problem_item': my_problem_item,'unread_messages':unread_messages})
 
 @login_required
 def problem_like(request, problem_detail_key_id):
