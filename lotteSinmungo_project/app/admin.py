b03169608e786629d1967a2c548d0678249db2b0
Gobans <audghks4452@gmail.com>
2020-10-18 12:19:56 +0900

    resolving #122

--- lotteSinmungo_project/app/admin.py
+++ lotteSinmungo_project/app/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from .models import Problem, myUser, Solution,Photo
+from .models import Problem, myUser, Solution
 from django.contrib.auth.admin import UserAdmin
 
-class PhotoInline(admin.TabularInline):
-    model = Photo
-
 # Register your models here.
-admin.site.register(Photo)
 admin.site.register(Solution)
 admin.site.register(Problem)
 admin.site.register(myUser, UserAdmin)
