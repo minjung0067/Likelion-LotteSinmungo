fe3f31f6d596bd86f2532578b728957b02b6009e
minjung <minjung0067@naver.com>
2020-10-23 22:51:57 +0900

    ss

--- lotteSinmungo_project/app/models.py
+++ lotteSinmungo_project/app/models.py
@@ -18,7 +18,6 @@ class myUser(AbstractUser):
     password = models.CharField(max_length=64,verbose_name = '비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,verbose_name='등록시간')
     like_problems = models.ManyToManyField('Problem', blank=True, related_name='like_users')
-    image = models.ImageField(upload_to='images/',blank=True, null=True)
     def __str__(self):
         return self.username  
 
