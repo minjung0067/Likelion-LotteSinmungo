2bb53d3939807f28a36bdcb4eb850b9186e44dc3
jaehoonkimm <jaehoonkim.dev@gmail.com>
2020-10-23 22:53:45 +0900

    fix: Add ALLOWED_HOSTS IP for connect to AWS Instance port

--- lotteSinmungo_project/lotteSinmungo/settings.py
+++ lotteSinmungo_project/lotteSinmungo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$b2zg1+7b1=_%@@j4+2eqda741+^^layw!^22m!bflih8$a0=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.125.197.94']
 
 
 # Application definition
